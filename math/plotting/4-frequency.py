e80498ad3959fecf08e0c1ba3fea7bfb24eda0d4
sonia250 <s.uwase3@alustudent.com>
2026-01-16 13:01:33 +0200

    Add histogram frequency plot task

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+# your code here
+plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+plt.xlim(0, 100)
+plt.xticks(range(0, 101, 10))
+plt.show()
