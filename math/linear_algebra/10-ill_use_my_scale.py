06b4da94e4dbdb66c9773400a34f470b80f69f05
sonia250 <s.uwase3@alustudent.com>
2026-01-30 18:21:50 +0200

    Add NumPy shape function

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module for calculating the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray
+
+    Args:
+        matrix: A numpy.ndarray
+
+    Returns:
+        A tuple of integers representing the shape
+    """
+    return matrix.shape
