b6c531fd9089b455d401b400b606a20a078d5a91
sonia250 <s.uwase3@alustudent.com>
2026-01-31 15:38:11 +0200

    Add array concatenation function

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for concatenating two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays
+
+    Args:
+        arr1: First array (list of ints/floats)
+        arr2: Second array (list of ints/floats)
+
+    Returns:
+        A new list containing elements from both arrays
+    """
+    return arr1 + arr2
