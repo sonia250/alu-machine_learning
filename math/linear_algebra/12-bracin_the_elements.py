d6a4eb768c556b4e0614ea7048736368a0d288f5
sonia250 <s.uwase3@alustudent.com>
2026-01-31 18:01:45 +0200

    Add function docstring

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -2,4 +2,5 @@
 
 
 def np_elementwise(mat1, mat2):
+    """Performs element-wise addition, subtraction, multiplication, and division"""
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
