c934638cdd99ec6c7529dd76a672ec0ae5f3546b
sonia250 <s.uwase3@alustudent.com>
2026-01-31 18:04:48 +0200

    Fix docstring length and add module docstring

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+"""Module for element-wise numpy array operations"""
 
 
 def np_elementwise(mat1, mat2):
-    """Performs element-wise addition, subtraction, multiplication, and division"""
+    """Performs element-wise add, sub, mul, and div"""
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
