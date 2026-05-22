03d51c8c69a781f3e32d93188f4560257827aeef
sonia250 <s.uwase3@alustudent.com>
2026-05-22 09:44:03 +0300

    Add NeuralNetwork Forward Propagation

---
+++ supervised_learning/classification/10-neural_network.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""Defines a neural network with forward propagation."""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """A neural network with one hidden layer performing binary classification."""
+
+    def __init__(self, nx, nodes):
+        """
+        Initialize the neural network.
+        
+        Args:
+            nx: Number of input features
+            nodes: Number of nodes found in the hidden layer
+            
+        Raises:
+            TypeError: If nx or nodes is not an integer
+            ValueError: If nx or nodes is less than 1
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+    
+    @property
+    def W1(self):
+        """Return the weights vector for the hidden layer."""
+        return self.__W1
+    
+    @property
+    def b1(self):
+        """Return the bias for the hidden layer."""
+        return self.__b1
+    
+    @property
+    def A1(self):
+        """Return the activated output for the hidden layer."""
+        return self.__A1
+    
+    @property
+    def W2(self):
+        """Return the weights vector for the output neuron."""
+        return self.__W2
+    
+    @property
+    def b2(self):
+        """Return the bias for the output neuron."""
+        return self.__b2
+    
+    @property
+    def A2(self):
+        """Return the activated output for the output neuron."""
+        return self.__A2
+    
+    def forward_prop(self, X):
+        """
+        Calculate the forward propagation of the neural network.
+        
+        Args:
+            X: numpy.ndarray with shape (nx, m) containing input data
+            
+        Returns:
+            The private attributes __A1 and __A2, respectively
+        """
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+        
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+        
+        return self.__A1, self.__A2
