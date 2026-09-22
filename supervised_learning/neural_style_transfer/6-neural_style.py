a69519dd394618f492145f2b9d0eb1c7fb50e13b
sonia250 <s.uwase3@alustudent.com>
2026-09-22 19:31:44 +0300

    Implement content cost for NST

--- supervised_learning/neural_style_transfer/6-neural_style.py
+++ supervised_learning/neural_style_transfer/6-neural_style.py
@@ -268,10 +268,12 @@ class NST:
                 contains content output for the generated image
 
         returns:
-            the style cost
+            the content cost
         """
         shape = self.content_feature.shape
         if not isinstance(content_output, (tf.Tensor, tf.Variable)) or \
            content_output.shape != shape:
             raise TypeError(
                 "content_output must be a tensor of shape {}".format(shape))
+
+        return tf.reduce_mean(tf.square(content_output - self.content_feature))
